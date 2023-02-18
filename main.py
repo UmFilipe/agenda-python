70476a29de0e1ce4bde984d03e449326686b4e20
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2023-02-18 20:48:39 -0300

    Função de criar melhorada e função de mostrar todos os compromissos finalizada

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-listaDeCompromissos = []
-
-class compromisso():
+class Compromisso:
     data = None
     hora = None
     duracao = None
     descricao = None
 
+listaDeCompromissos = []
+
 def criaCompromisso(vetor):
-    comp = compromisso()
+    comp = Compromisso()
 
     comp.data = input("Digite a data do compromisso (dd/mm/aaaa): ")
     comp.hora = input("Digite a hora do compromisso: ")
     comp.duracao = input("Digite a duração em horas do compromisso: ")
     comp.descricao = input("Digite a descrição para seu compromisso: ")
 
-    vetor.append(comp.data)
-    vetor.append(comp.hora)
-    vetor.append(comp.duracao)
-    vetor.append(comp.descricao)
+    vetor.append(comp)
+    menu()
     return vetor
 
 def consultarCompromisso():
     # função de consulta
     menu()
+
 def editarCompromisso():
     #função de edição
     menu()
@@ -31,12 +30,25 @@ def excluirCompromisso():
     #função de exclusão
     menu()
 
-def mostraComprpmisso(vetor):
-    print(vetor)
+def templateImpressão(compromisso):
+    print(f"Data do compromisso: {compromisso.data}")
+    print(f"Hora do compromisso: {compromisso.hora}")
+    print(f"Duração do compromisso: {compromisso.duracao} horas")
+    print(f"Descrição do compromisso: {compromisso.descricao}")
+    print("-----------------------------------")
+
+def mostraCompromisso(vetor):
+    if vetor:
+        for i in range(len(vetor)):
+            templateImpressão(vetor[i])
+    else:
+        print("Não existem eventos na sua agenda!")
+    
     menu()
+    
 
 def menu():
-    print("Bem vindo a sua agenda!")
+    print("----------------------------")
     print("Opções da agenda:")
     print("Opção 1: Criar compromisso")
     print("Opção 2: Consultar compromissos")
@@ -44,22 +56,26 @@ def menu():
     print("Opção 4: Excluir compromisso")
     print("Opção 5: Mostrar todos os seus compromissos")
     print("Opção 6: Sair")
+    print("------------------------------")
 
     opcao = int(input("Digite uma opção: "))
 
     if opcao == 1:
         criaCompromisso(listaDeCompromissos)
-    if opcao == 2:
+    elif opcao == 2:
         consultarCompromisso()
-    if opcao == 3:
+    elif opcao == 3:
         editarCompromisso()
-    if opcao == 4:
+    elif opcao == 4:
         excluirCompromisso()
-    if opcao == 5:
-        mostraComprpmisso(listaDeCompromissos)
-    if opcao == 6:
+    elif opcao == 5:
+        mostraCompromisso(listaDeCompromissos)
+    elif opcao == 6:
         print("Obrigado por usar a agenda!")
         print("Desenvolvido por Filipe Medeiros de Almeida")
+    else:
+        print("Opção inválida!")
+        menu()
 
 menu()
 
